1273f09225c581dac9124e1f2d4e3efb3d025ea9
ieexiy <73919435+ieexiy@users.noreply.github.com>
2020-11-05 03:14:59 +0100

    Update v1_1_alternativer code.m

--- Lab 1/v1_1_alternativer code.m
+++ Lab 1/v1_1_alternativer code.m
@@ -8,8 +8,8 @@
 %
 % Abgabe Donnerstag, 5.11.2020
 
-clc; % Kommandofenster bereinigen
-clear; % Variablen bereinigen
+clc;       % Kommandofenster bereinigen
+clear;     % Variablen bereinigen
 close all; % Alles schließen
 
 % Parameter
@@ -19,30 +19,30 @@ td = 2     % Signaldauer
 a = 2      % Amplitude (Lautstärke)
 
 % Berechnung
-T = 1/fa           % Abtastperiode
-tVec = 0 : T : td; % Zeitvektor
+T = 1/fa                             % Abtastperiode
+tVec = 0 : T : td;                   % Zeitvektor
 sinVec = a * sin(2 * pi * f * tVec); % Sinusformel
 
 % Ausgabe
 
 % Komplette 2 Sekunden Ausgabe
-figure(1)
-subplot(3, 1, 1)
-plot(tVec, sinVec);
+figure(1) % Fenster
+subplot(3, 1, 1) % Graphenanordnung
+plot(tVec, sinVec); % Sinus wird erzeugt im Fenster
 xlabel('Zeit in Sekunden'); % x-Achse wird beschriftet
 ylabel('Amplitude'); % y-Achse wird beschriftet
 title('Komplette Sinus Ausgabe'); % Titel des Graphs
 
 % Halbe Sekunde
-subplot(3, 1, 2)
-plot(tVec(1:td*fa/4), sinVec(1:td*fa/4));
+subplot(3, 1, 2) % Graphenanordnung
+plot(tVec(1:td*fa/4), sinVec(1:td*fa/4)); % Sinus wird erzeugt im Fenster
 xlabel('Zeit in Sekunden'); % x-Achse wird beschriftet
 ylabel('Amplitude'); % y-Achse wird beschriftet
 title('Eine halbe Sekunde'); % Titel des Graphs
 
 % Ein Sinuszyklus
-subplot(3, 1, 3)
-plot(tVec(1:12), sinVec(1:12));     % Regulärer Sinus wird erzeugt
+subplot(3, 1, 3) % Graphenanordnung
+plot(tVec(1:12), sinVec(1:12)); % Sinus wird erzeugt im Fenster
 %plot(tVec(1:12), sinVec(1:12), '*'); % Einzelne Abtastungen werden angezeigt
 xlabel('Zeit in t/s'); % x-Achse wird beschriftet
 ylabel('Amplitude'); % y-Achse wird beschriftet
