b6beaafcbba2501836f3c07755f208098ce20496
Johannes <johannes.bluhm@outlook.com>
2020-11-05 15:09:35 +0100

    Kleine Fehler, Aufgabenreihenfolge

--- Lab 1/FabianAndresen_JohannesBluhm_v1_1.m
+++ Lab 1/FabianAndresen_JohannesBluhm_v1_1.m
@@ -13,18 +13,23 @@ clear;     % Variablen bereinigen
 close all; % Alles schließen
 
 % Parameter
-fa = 11025 % Abtastrate
-f = 1000   % Signalfrequenz
-td = 2     % Signaldauer
-a = 2      % Amplitude (Lautstärke)
+fa = 11025; % Abtastrate
+f = 1000;   % Signalfrequenz
+td = 2;     % Signaldauer
+a = 2;      % Amplitude (Lautstärke)
 
 % Berechnung
-T = 1/fa                             % Abtastperiode
+T = 1/fa;                             % Abtastperiode
 tVec = 0 : T : td;                   % Zeitvektor
 sinVec = a * sin(2 * pi * f * tVec); % Sinusformel
 
-% Ausgabe
+% Aufgabe 2.2 Signal mit sound Befehl ausgeben
+%sound (sinVec, 4000);
+%sound (sinVec);
+%sound (sinVec, fa);
+%sound (sinVec, 2 * fa);
 
+% Aufgabe 2.3
 % Komplette 2 Sekunden Ausgabe
 figure(1) % Fenster
 subplot(3, 1, 1) % Graphenanordnung
@@ -33,6 +38,7 @@ xlabel('Zeit in Sekunden'); % x-Achse wird beschriftet
 ylabel('Amplitude'); % y-Achse wird beschriftet
 title('Komplette Sinus Ausgabe'); % Titel des Graphs
 
+% Aufgabe 2.4
 % Halbe Sekunde
 subplot(3, 1, 2) % Graphenanordnung
 plot(tVec(1:td*fa/4), sinVec(1:td*fa/4)); % Sinus wird erzeugt im Fenster
@@ -47,9 +53,3 @@ plot(tVec(1:12), sinVec(1:12)); % Sinus wird erzeugt im Fenster
 xlabel('Zeit in t/s'); % x-Achse wird beschriftet
 ylabel('Amplitude'); % y-Achse wird beschriftet
 title('Ein Sinuszyklus'); % Titel des Graphs
-
-% Aufgabe 2.2 Signal mit sound Befehl ausgeben
-%sound (sinVec, 4000);
-%sound (sinVec);
-%sound (sinVec, fa);
-%sound (sinVec, 2 * fa);
