3966181c40986cdea0b62ab3cae93e41922ec328
ieexiy <73919435+ieexiy@users.noreply.github.com>
2020-12-08 00:02:54 +0100

    Update FabianAndresen_JohannesBluhm_v4_3.m

--- Lab 4/FabianAndresen_JohannesBluhm_v4_3.m
+++ Lab 4/FabianAndresen_JohannesBluhm_v4_3.m
@@ -23,8 +23,8 @@ freq2 = 1209; % Frequenz 2
 % Berechnung
 t = 1/fa;              % Abtastperiode
 tVec = 0 : t : td - t; % Zeitvektor
-cosVec1 = a * cos(2 * pi * freq1 * tVec + 7.85); % Sinus 1
-cosVec2 = a * 1.25 * cos(2 * pi * freq2 * tVec + 7.85); % Sinus 2
+cosVec1 = a * cos(2 * pi * freq1 * tVec - pi/2); % Sinus 1
+cosVec2 = a * 1.25 * cos(2 * pi * freq2 * tVec - pi/2); % Sinus 2
 
 pauseVec = zeros(1 , length(tVec)); % Pausenvektor
 cosVec = cosVec1 + cosVec2; % Vektoren addieren
@@ -43,10 +43,12 @@ figure(2); % Neues Fenster
 
 subplot(2, 1, 1);
 plot(tVec(1:110), cosVec(1:110), '-r'); % Ausgabe
-xlabel('Zeit in ms'); % x-Achse wird beschriftet
+xlabel('Zeit in Sekunden'); % x-Achse wird beschriftet
 ylabel('Amplitude'); % y-Achse wird beschriftet
+title('Die ersten 10ms'); % Titel des Graphs
 
 subplot(2, 1, 2);
 plot(tVec2(1:length(rep)), rep(1:length(rep)), '-r'); % Ausgabe
 xlabel('Zeit in Sekunden'); % x-Achse wird beschriftet
 ylabel('Amplitude'); % y-Achse wird beschriftet
+title('Signal und Pause'); % Titel des Graphs
