cd660dc31f1a8ae4df7d01dc44df2546b0c5263c
ieexiy <73919435+ieexiy@users.noreply.github.com>
2021-01-06 00:14:02 +0100

    Create FabianAndresen_JohannesBluhm_v9_1.m

---
+++ Lab 9/FabianAndresen_JohannesBluhm_v9_1.m
@@ -0,0 +1,39 @@
+% Rücktransformation in den Zeitbereich
+%
+% Fabian Andresen und Johannes Bluhm 05.01.2021
+%
+% MATLAB R2019b
+%
+% Arbeiten mit der Funktion funcRidft, Zusammenhang zwischen Transformation und Rücktransformation erkennen
+%
+% Abgabe Montag, 11.01.2021
+
+clc;       % Kommandofenster bereinigen
+clear;     % Variablen bereinigen
+close all; % Alles schließen
+
+% Parameter
+fa = 11025;  % Abtastrate
+td = 1;    % Signaldauer
+amp = 1;     % Amplitude (Lautstärke)
+freq = 1000;  % Frequenz
+
+t = 1/fa;              % Abtastperiode
+tVec = 0 : t : td - t; % Zeitvektor
+recVec = zeros(1, td * fa); % Vorbereiteter Vector
+
+recVec (floor(0.1*fa) : floor(0.3*fa)) = ones;              % Vektor mit Einsen füllen
+[freqVec, phaVec] = funcRdft(recVec, length(recVec));       % Transformieren mittels funcRdft
+
+figure(1);
+subplot(2, 1, 1);
+plot(tVec, recVec);                                         % Ausgabe Signal im Zeitbereich
+xlabel('Zeit t/s');                                         % x-Achse wird beschriftet
+ylabel('Amplitude');                                        % y-Achse wird beschriftet
+title('Rechteck im Zeitbereich');
+
+subplot(2, 1, 2);
+plot(freqVec(1:20));                                        % Ausgabe Signal im Frequenzbereich
+xlabel('Frequenz Hz');                                      % x-Achse wird beschriftet
+ylabel('Amplitude');                                        % y-Achse wird beschriftet
+title('Rechteck im Frequenzbereich mit einer oberen Frequenz von 20');
