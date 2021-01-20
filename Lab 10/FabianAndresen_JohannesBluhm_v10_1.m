37a3af8ae995a0ddfb1fba4bb07682a2773c3e26
ieexiy <73919435+ieexiy@users.noreply.github.com>
2021-01-20 08:33:57 +0100

    Update FabianAndresen_JohannesBluhm_v10_1.m

--- Lab 10/FabianAndresen_JohannesBluhm_v10_1.m
+++ Lab 10/FabianAndresen_JohannesBluhm_v10_1.m
@@ -26,5 +26,19 @@ tVec = 0 : t : td - t; % Zeitvektor
 cosVec1 = a * cos(2 * pi * freq1 * tVec - pi/2); % Sinus 1
 cosVec2 = a * 1.25 * cos(2 * pi * freq2 * tVec - pi/2); % Sinus 2
 
-pauseVec = zeros(1 , length(tVec)); % Pausenvektor
 cosVec = cosVec1 + cosVec2; % Vektoren addieren
+
+fr1 = freq1/(fa/2);
+fr2 = freq2/(fa/2);
+
+lowpass = fir1(96,[fr1],'low');
+freqz(lowpass,a);
+final = filter(lowpass,a,cosVec);
+
+subplot(2,1,1)
+func_fDarstellung(cosVec, fa, 2000);
+%sound(cosVec);
+
+subplot(2,1,2)
+func_fDarstellung(final, fa, 2000);
+%sound(final);
