0db19981c37a7aae2f3beda5409568ec2799410f
Johannes <johannes.bluhm@outlook.com>
2021-01-20 17:00:47 +0100

    Update FabianAndresen_JohannesBluhm_v10_1.m

--- Lab 10/FabianAndresen_JohannesBluhm_v10_1.m
+++ Lab 10/FabianAndresen_JohannesBluhm_v10_1.m
@@ -30,24 +30,26 @@ cosVec = cosVec1 + cosVec2; % Vektoren addieren
 
 % --- 2.1 --- %
 
-fr1 = (freq1+100)/(fa/2);         % Grenzfrequenz 1 (+100 damit er weniger von der unteren Frequenz filtert)
-fr2 = freq2/(fa/2);               % Grenzfrequenz 2
+fr1 = (freq1+100)/(fa/2);               % Grenzfrequenz 1 (+100 damit er weniger von der unteren Frequenz filtert)
+fr2 = freq2/(fa/2);                     % Grenzfrequenz 2
 
-lowpass = fir1(96,[fr1],'low');   % fir1 wird benutzt
-final = filter(lowpass,a,cosVec); % Filter wird auf cosVec angewandt
+lowpass = fir1(96,[fr1],'low');         % fir1 wird benutzt
+final = filter(lowpass,a,cosVec);       % Filter wird auf cosVec angewandt
 
 figure(1);
-subplot(3,1,1)                    % Vor dem Filter
-%func_fDarstellung(cosVec, fa, 2000);
+subplot(3,1,1)                          
+func_fDarstellung(cosVec, fa, 2000);    % Vor dem Filter
+title('Ton vor dem Filter');
 %sound(cosVec);
 
-subplot(3,1,2)                    % Nach dem Filter
-%func_fDarstellung(final, fa, 2000);
+subplot(3,1,2)                    
+func_fDarstellung(final, fa, 2000);     % Nach dem Filter
+title('Ton nach dem Filter');
 %sound(final);
 
-subplot(3,1,3)                    % Hilft zum erkennen des Filters an sich
-%func_fDarstellung(lowpass, fa, 2000);
-%title('Grenzfrequenz wird visualisiert');
+subplot(3,1,3)                    
+func_fDarstellung(lowpass, fa, 2000);   % Hilft zum erkennen des Filters an sich
+title('Grenzfrequenz wird visualisiert');
 
 % --- 2.2 --- %
 
@@ -57,30 +59,31 @@ subplot(3,1,3)                    % Hilft zum erkennen des Filters an sich
 % Beim verändern der Anzahl der Koeffizienten bestimmt man wie schnell
 % diese Kurve abflacht. Sprich wie harsch der Filter sich auswirkt.
 
-wav = audioread('/S_dlma.wav')';    % file einlesen
-fr3 = 800/(fa/2);                   % Grenzfrequenz 3
-lowpass2 = fir1(442,[fr3],'low');   % fir1 wird benutzt
-finalWav = filter(lowpass2,a,wav);  % Filter wird auf wav angewandt
+wav = audioread('/S_dlma.wav')';        % file einlesen
+fr3 = 800/(fa/2);                       % Grenzfrequenz 3
+lowpass2 = fir1(442, [fr3], 'low');     % fir1 wird benutzt
+finalWav = filter(lowpass2, a, wav);    % Filter wird auf wav angewandt
 
 figure(2);
-subplot(4, 1, 1);                   % Nach dem Filter
-func_fDarstellung(wav, fa, 2000);   % Ausgabe Sprachsignal im Frequenzbereich vor Filter
+subplot(4, 1, 1);                       % Vor dem Filter
+func_fDarstellung(wav, fa, 2000);       % Ausgabe Sprachsignal im Frequenzbereich vor Filter
 title('Wave File ohne Filter im Frequenzbereich')
 
-subplot(4, 1, 2);                   % Nach dem Filter
-func_fDarstellung(finalWav, fa, 2000);   % Ausgabe Sprachsignal im Frequenzbereich nach Filter
-%TODO: Geben sie beim gefilterten Sprachsignal die von ihnen gewählte Grenzfrequenz und die Anzahl der Koeffizienten mit im Titel aus. 
+subplot(4, 1, 2);                       % Nach dem Filter
+func_fDarstellung(finalWav, fa, 2000);  % Ausgabe Sprachsignal im Frequenzbereich nach Filter
 title('Grenzfrequenz = 800; Koeffizienten = 442');
 
 subplot(4, 1, 3);
-plot(wav);                                       % Ausgabe Sprachsignal im Zeitbereich vor Filter
-xlabel('Zeit t/s');                                         % x-Achse wird beschriftet
-ylabel('Amplitude');                                        % y-Achse wird beschriftet
+plot(wav);                               % Ausgabe Sprachsignal im Zeitbereich vor Filter
+%TODO: Zeitvektor
+xlabel('Zeit s');                        % x-Achse wird beschriftet
+ylabel('Amplitude');                     % y-Achse wird beschriftet
 title('Wave File ohne Filter im Zeitbereich')
 
 subplot(4, 1, 4);
-plot(finalWav);                                       % Ausgabe Sprachsignal im Zeitbereich nach Filter
-xlabel('Zeit t/s');                                         % x-Achse wird beschriftet
-ylabel('Amplitude');                                        % y-Achse wird beschriftet
+plot(finalWav);                          % Ausgabe Sprachsignal im Zeitbereich nach Filter
+%TODO: Zeitvektor
+xlabel('Zeit s');                        % x-Achse wird beschriftet
+ylabel('Amplitude');                     % y-Achse wird beschriftet
 title('Wave File mit Filter; Grenzfrequenz = 800; Koeffizienten = 442');
 %sound(finalWav);
